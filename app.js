cf2a72963002c5bd558acd3bb8cf2af7e14dd1d5
Jorge Tapia <283672+itsprof@users.noreply.github.com>
2020-04-03 14:48:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = process.env.PORT || 5000;
 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Prof!');
+}).listen(port, host, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
