cddbaeff01f8b2906d3ae92c8a2db0b59069d54f
Jorge Tapia <jorge@itsprof.dev>
2020-04-03 14:59:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Prof!');
-}).listen(port, host, () => {
+}).listen(() => {
   console.log('Server is now running...');
 });
